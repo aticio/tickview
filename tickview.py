ac81783bccc78be4719d2a04d3a0886fc4f0a8ef
Özgür Atıcı <35876648+aticio@users.noreply.github.com>
2022-08-03 14:20:33 +0000

    initial commit

---
+++ tickview.py
@@ -0,0 +1,58 @@
+import json
+import websocket
+import logging
+import os
+from datetime import datetime
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+
+token = os.getenv("INFLUX_TOKEN")
+org = "aticio-org"
+bucket = "tickview"
+
+def main():
+    init_stream()
+
+
+# Websocket functions
+def init_stream():
+    websocket.enableTrace(True)
+    w_s = websocket.WebSocketApp(
+        "wss://stream.binance.com:9443/ws/!miniTicker@arr",
+        on_message=on_message,
+        on_error=on_error,
+        on_close=on_close
+        )
+    w_s.on_open = on_open
+    w_s.run_forever()
+
+
+def on_error(w_s, error):
+    logging.error(error)
+
+
+def on_close(w_s):
+    logging.info("closing websocket connection, initiating again...")
+    init_stream()
+
+
+def on_open(w_s):
+    logging.info("websocket connection opened...")
+
+
+def on_message(w_s, message):
+    ticker_data = json.loads(message)
+    sequence = []
+    for t in ticker_data:
+        if "BUSD" in t["s"]:
+            sequence.append(f"tick,symbol={t['s']} price={t['c']},volume={t['v']}")
+
+    with InfluxDBClient(url="http://localhost:8086", token=token, org=org) as client:
+        write_api = client.write_api(write_options=SYNCHRONOUS)
+        write_api.write(bucket, org, sequence)
+        client.close()
+
+
+if __name__ == "__main__":
+    main()
